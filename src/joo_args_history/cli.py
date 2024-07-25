7f53253807c6fdd7dc6a2546ceb095b2ed69d9cd
baechu805 <bjoo606@naver.com>
2024-07-25 14:46:47 +0900

    a

--- src/joo_args_history/cli.py
+++ src/joo_args_history/cli.py
@@ -29,6 +29,7 @@ def cmd():
             # TODO 특정 날짜의 명령어 TOP N
         else:
             print("TODO - 에러나 안내메세지를 주면")
+            parser.error("-t 옵션은 -d 옵션과 함께 사용하시오!")
     else:
         # TODO - 사용법을 출력한다
         parser.print_help()
